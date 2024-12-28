a1150048ce8596e40748281f8473936151ec972e
Souchy <robyn.g@hotmail.fr>
2024-12-28 14:50:07 -0500

    Update Components3d.h

--- projects/engine/src/main/public/components/3d/Components3d.h
+++ projects/engine/src/main/public/components/3d/Components3d.h
@@ -24,6 +24,20 @@ struct Velocity
 class Transform3d
 {
 public:
+	Transform3d()
+	{
+		position = glm::vec3(0.0f);
+		rotation = glm::vec3(0.0f);
+		scale = glm::vec3(1.0f);
+		update();
+	}
+	Transform3d(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale)
+	{
+		this->position = position;
+		this->rotation = rotation;
+		this->scale = scale;
+		update();
+	}
 	void setPosition(glm::vec3 position)
 	{
 		this->position = position;
